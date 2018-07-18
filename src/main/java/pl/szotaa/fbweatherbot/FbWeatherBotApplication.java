ae3fc7a6d98100378246ebb8ea18f83c80280980
szotaa <kubaszota97@gmail.com>
2018-07-18 13:44:54 +0200

    initial spring boot application

---
+++ src/main/java/pl/szotaa/fbweatherbot/FbWeatherBotApplication.java
@@ -0,0 +1,12 @@
+package pl.szotaa.fbweatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbWeatherBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbWeatherBotApplication.class, args);
+	}
+}
