99d4785c0e19b5601e1c7bbb7d1481d24922dde7
szotaa <kubaszota97@gmail.com>
2018-07-19 12:38:39 +0200

    add logging for advanced debugging

--- src/main/java/pl/szotaa/fbweatherbot/facebook/controller/MessengerController.java
+++ src/main/java/pl/szotaa/fbweatherbot/facebook/controller/MessengerController.java
@@ -3,6 +3,7 @@ package pl.szotaa.fbweatherbot.facebook.controller;
 import com.github.messenger4j.Messenger;
 import com.github.messenger4j.exception.MessengerVerificationException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.szotaa.fbweatherbot.facebook.service.MessengerService;
 
+@Slf4j
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
@@ -26,6 +28,7 @@ public class MessengerController {
             @RequestParam(Messenger.VERIFY_TOKEN_REQUEST_PARAM_NAME) String token,
             @RequestParam(Messenger.CHALLENGE_REQUEST_PARAM_NAME) String challenge) throws MessengerVerificationException {
 
+        log.info("controller received verification with mode: " + mode + " token: " + token + " challenge: " + challenge);
         this.messengerService.verifyWebHook(mode, token);
         return ResponseEntity.ok(challenge);
     }
@@ -35,6 +38,7 @@ public class MessengerController {
             @RequestBody String payload,
             @RequestHeader(Messenger.SIGNATURE_HEADER_NAME) String signature) throws MessengerVerificationException {
 
+        log.info("controller received payload: " + payload + " with signature: " + signature);
         this.messengerService.handleMessage(payload, signature);
         return ResponseEntity.ok().build();
     }
